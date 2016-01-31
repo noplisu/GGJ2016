61dfb1c583c4193aaca4cce5535c558475f228b0
GLisowski <glisowski@fractalsoft.org>
2016-01-31 16:38:05 +0100

    first web version

--- Assets/Rotate.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Rotate : MonoBehaviour {
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(0, 0, 50 * Time.deltaTime);
-    }
-}
--- Assets/Scripts/hurtPlayer.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class hurtPlayer : MonoBehaviour {
-    public float amount = 10;
-}
